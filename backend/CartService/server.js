3df3a2ea001e2e1d2973ff9695bb83dcce04da8f
Deshan Rajapaksha <98569506+Desh19@users.noreply.github.com>
2023-04-27 07:48:53 +0530

    backend changes
    backend changes

---
+++ backend/CartService/server.js
@@ -0,0 +1,39 @@
+const express =  require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require('path');
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5002;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL=process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+  
+   useNewUrlParser:true,
+   useUnifiedTopology:true,
+
+});
+
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+    console.log("MongoDB Connected 🍀");
+});
+
+const Cart = require('./routes/Cart');
+app.use("/Cart",Cart);
+
+
+app.listen(PORT,()=>{
+    console.log(`Cart Service is up and running on port ${PORT} ✨`);
+})
+
+
+
---
+++ backend/LoginRegisterService/server.js
@@ -0,0 +1,35 @@
+const express =  require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5004;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL=process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+  
+   useNewUrlParser:true,
+   useUnifiedTopology:true,
+
+
+});
+
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+    console.log("MongoDB Connected 🍀");
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`LoginRegister Service is up and running on port ${PORT} ✨`);
+})
+
+
